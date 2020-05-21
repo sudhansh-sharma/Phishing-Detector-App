0b47daadd4219bcc52822f9baae25c6419a6a3a2
Sudhansh Sharma <62994831+Jaugor7@users.noreply.github.com>
2020-05-21 22:51:49 +0530

    Update settings.py

--- minorProjectApp/settings.py
+++ minorProjectApp/settings.py
@@ -23,7 +23,7 @@ TEMPALTE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7sect34l!ly9^@v(=0c@&$w^4^q0c6jpf)jne7#^4sbnc*lv+&'
+SECRET_KEY = "Enter Your Own Key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
